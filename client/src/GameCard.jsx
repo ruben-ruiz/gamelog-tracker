d5bc1d05307c61eeff873f2dad0a853644b624e6
Ruben <ruben@rubens-MacBook-Pro.local>
2021-05-19 10:29:55 -0400

    styled cards on display

--- client/src/GameCard.jsx
+++ client/src/GameCard.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  CardTitle, Button
 } from 'reactstrap';
 
 const GameCard = ({game}) => {
@@ -9,13 +9,13 @@ const GameCard = ({game}) => {
 
   return (
     <Card>
-      <CardImg top width="100%" src={game.background_image} alt="Card image cap" />
+      <CardImg src={game.background_image} alt="Card image cap" />
       <CardBody>
         <CardTitle tag="h5">{game.name}</CardTitle>
         <CardText>Released: {game.released}</CardText>
         <CardText>Rating: {game.rating}/{game.rating_top}</CardText>
         <CardText>{(game.genres).map(genre => genre.name).join(', ')}</CardText>
-        <Button className={`card-btn ${backlog ? 'card-btn-true': 'card-btn-false'}`} onClick={() => setBacklog(!backlog)}>Button</Button>
+        <Button className={`card-btn ${backlog ? 'card-btn-true': 'card-btn-false'}`} onClick={() => setBacklog(!backlog)}>{backlog ? 'Remove from Backlog' : 'Add to Backlog'}</Button>
       </CardBody>
     </Card>
   );
