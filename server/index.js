82ef0ea2293dca55d579ca7fb17c7da0a9ffc544
Ruben <ruben@rubens-MacBook-Pro.local>
2021-06-23 01:50:27 -0400

    fixed path for static build folder again

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ let ax = axios.create({
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static('client/build'));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // session
